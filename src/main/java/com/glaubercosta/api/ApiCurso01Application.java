aaa05085b1dde28c9ab5a376b16d35eb52a669df
Gláuber da Silva Costa <glaubscosta@gmail.com>
2019-03-28 19:53:45 -0300

    versao inicial do projeto

---
+++ src/main/java/com/glaubercosta/api/ApiCurso01Application.java
@@ -0,0 +1,13 @@
+package com.glaubercosta.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCurso01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCurso01Application.class, args);
+	}
+
+}
